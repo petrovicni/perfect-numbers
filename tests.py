cad520d0fb12b6a1163425dd69cb012cad557030
petrovicni <143907348+petrovicni@users.noreply.github.com>
2023-10-09 13:32:36 +0200

    x

---
+++ tests.py
@@ -0,0 +1,27 @@
+def test(x: int):
+    if x % 2:
+        l = 0
+    else:
+        r = x // 2
+        a = 0
+        while True:
+
+            print(f"Schleife ist: {r}")
+
+            if int(r) == 0:
+                if a == x:
+                    print(f"Die Zahl: {x} ist eine Komplette Zahl!")
+                    break
+                else:
+                    break
+            
+            else:
+                if x % r == 0:
+                    print(f"Rest von {x}:{r} = 0")
+                    a += r
+                    r -= 1
+                else:
+                    r -= 1
+                    continue
+
+test(28)
