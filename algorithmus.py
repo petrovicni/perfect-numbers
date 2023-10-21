c40768b4873868cfef32f25d9aeceed44a8ac898
petrovicni <143907348+petrovicni@users.noreply.github.com>
2023-10-21 15:23:57 +0200

    max speed

--- algorithmus.py
+++ algorithmus.py
@@ -1,30 +1,37 @@
-def komplettezahl(x: int):
-    if x % 2:
-        l = 0
+# fastest i could come up with using python
+from concurrent.futures import ThreadPoolExecutor
+
+biggest = 100**2149  # biggest number python can calculate with
+threads = 500        # max checks at once your pc can handle
+
+def perfectnum(number: int):
+    if number % 2:
+        return
     else:
-        r = x // 2
-        a = 0
+        indicator = number // 2
+        holder = 0
         while True:
 
-            if int(r) == 0:
-                if a == x:
-                    print(x)
+            if int(indicator) == 0:
+                if holder == number:
+                    open("perfect.txt", "a+").write(f"{number}\n")
                     break
                 else:
                     break
             
             else:
-                if x % r == 0:
-                    a += r
-                    r -= 1
+                if number % indicator == 0:
+                    holder += indicator
+                    indicator -= 1
                 else:
-                    r -= 1
+                    indicator -= 1
                     continue
 
-def main():
-    i = 0
-    while True:
-        komplettezahl(i)
-        i += 1
+def threading():
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        futures = [executor.submit(perfectnum, i) for i in range(1, biggest)]
+        for future in futures:
+            future.result()
 
-main()
+if __name__ == "__main__":
+    threading()
